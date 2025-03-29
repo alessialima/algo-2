4ea65062d53c7ec18dd46216548013f36c92f82a
alessia <lima.alessialourdes@gmail.com>
2025-03-28 22:14:04 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- ale/taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
+++ ale/taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
@@ -1,30 +1,43 @@
 package aed;
 
 class ArregloRedimensionableDeRecordatorios {
+    private Recordatorio i; 
+    private int[] array;
 
     public ArregloRedimensionableDeRecordatorios() {
-        // Implementar
+        this.i = new Recordatorio(i);
+        this.array = array;  // como defino un array wtf 
     }
 
     public int longitud() {
-        
+        res = array.length;
+        return res;
     }
 
     public void agregarAtras(Recordatorio i) {
-        // Implementar
+        int[] array2 = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[i];
+        }
+        array2[array2.length - 1] = i; 
+        return array2;
     }
 
     public Recordatorio obtener(int i) {
-        // Implementar
-        return null;
+        int pos = i;
+        return array[pos];
     }
 
     public void quitarAtras() {
-        // Implementar
+        int[] array2 = new int[array.length - 1];
+        for (int i = 0; i < array2.length; i++){
+            array2[i] = array[i]; 
+        }
+        return array2;
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
-        // Implementar
+        // no entiendo qué pide 
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
