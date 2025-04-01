c7807e0c132137c3fecf38c43af95cfc1a2f486f
alessia <lima.alessialourdes@gmail.com>
2025-04-01 17:31:18 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- Taller 2/ArregloRedimensionableDeRecordatorios.java
+++ Taller 2/ArregloRedimensionableDeRecordatorios.java
@@ -1,43 +1,39 @@
 package aed;
 
 class ArregloRedimensionableDeRecordatorios {
-    private Recordatorio i; 
-    private int[] array;
+    private int longitud; 
+    private Recordatorio[] recordatorio;
 
     public ArregloRedimensionableDeRecordatorios() {
-        this.i = new Recordatorio(i);
-        this.array = array;  // como defino un array wtf 
+        this.longitud = 0;
+        this.recordatorio = Recordatorio[0];
     }
 
     public int longitud() {
-        res = array.length;
-        return res;
+        longitud = this.recordatorio.length;
+        return longitud;
     }
 
     public void agregarAtras(Recordatorio i) {
-        int[] array2 = new int[array.length + 1];
-        for (int i = 0; i < array.length; i++) {
-            array2[i] = array[i];
+        int longitud2 = longitud();
+        Recordatorio[] recordatorio_new = new Recordatorio[longitud() + 1];
+        for (int j = 0; j < longitud2; j++){
+            recordatorio_new[j] = this.recordatorio[j];
+            longitud += 1;
         }
-        array2[array2.length - 1] = i; 
-        return array2;
+        recordatorio_new[longitud] = i;
     }
 
     public Recordatorio obtener(int i) {
-        int pos = i;
-        return array[pos];
+        return recordatorio[i];
     }
 
     public void quitarAtras() {
-        int[] array2 = new int[array.length - 1];
-        for (int i = 0; i < array2.length; i++){
-            array2[i] = array[i]; 
-        }
-        return array2;
+        
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
-        // no entiendo qué pide 
+        this.recordatorio[indice] = valor;
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
