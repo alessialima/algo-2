9207877706be674a75290c8da95ebfb71384cc12
alessia <lima.alessialourdes@gmail.com>
2025-04-01 16:09:21 -0300

    Update Agenda.java

--- Taller 2/Agenda.java
+++ Taller 2/Agenda.java
@@ -1,30 +1,31 @@
 package aed;
 
 public class Agenda {
-    private Fecha fechaActual;
+    private Fecha fechaActual; 
     private Recordatorio recordatorio;
 
     public Agenda(Fecha fechaActual) {
-        
+        this.fechaActual = new Fecha(fechaActual);
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
-        // agrega un recordatorio (ya tengo una fucnion que agrega cosas en arreglos etc)
+        
     }
 
     @Override
     public String toString() {
-        // tengo una secuencia con tres recordatorios. puedo hacer to string con cada recordatorio
-        // fecha actual + ===== + recordatorio 1 + recordatorio 2 + recordatorio 3 
-        return "";
+        String res = fechaActual.toString() + "=====" + recordatorio.toString();
+        return res; 
     }
 
     public void incrementarDia() {
-        // llamo a la otra funcion incrementar dia pero seria con fecha actual 
+        
+
     }
 
     public Fecha fechaActual() {
-        // y este que es ??? 
+        // Implementar
+        return null;
     }
 
 }
