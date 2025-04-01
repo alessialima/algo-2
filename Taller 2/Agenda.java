2afa563423b6c32a3c3928e6ec7f92f9d01c6494
alessia <lima.alessialourdes@gmail.com>
2025-04-01 18:35:19 -0300

    Update Agenda.java

--- Taller 2/Agenda.java
+++ Taller 2/Agenda.java
@@ -1,51 +1,30 @@
 package aed;
 
+public class Agenda {
+    private Fecha fechaActual;
+    private Recordatorio recordatorio;
+    private ArregloRedimensionableDeRecordatorios agenda;
 
-class ArregloRedimensionableDeRecordatorios {
-    private int longitud;
-    private Recordatorio[] recordatorio;
-
-    public ArregloRedimensionableDeRecordatorios() {
-        this.recordatorio = new Recordatorio[0];
-        this.longitud = 0;
+    public Agenda(Fecha fechaActual) {
+        this.fechaActual = new Fecha(fechaActual);
+        this.agenda = new ArregloRedimensionableDeRecordatorios(agenda);
     }
 
-    public int longitud() {
-        return this.longitud;
+    public void agregarRecordatorio(Recordatorio recordatorio) {
+        this.agenda.agregarAtras(recordatorio);
     }
 
-    public void agregarAtras(Recordatorio i) {
-        Recordatorio[] recordatorio2 = new Recordatorio[recordatorio.length + 1];
-        for (int j = 0; j < recordatorio.length; j++) {
-            recordatorio2[j] = this.obtener(j);
-        }
-        recordatorio2[recordatorio2.length - 1] = i; 
-        recordatorio = recordatorio2;
+    @Override
+    public String toString() {
+        return fechaActual.toString() + "=====" + recordatorio.toString();
     }
 
-    public Recordatorio obtener(int i) {
-        return recordatorio[i];
+    public void incrementarDia() {
+        this.fechaActual.incrementarDia();
     }
 
-    public void quitarAtras() {
-        Recordatorio[] recordatorio2 = new Recordatorio[recordatorio.length - 1];
-        for (int j = 0; j < (recordatorio.length-1); j++) {
-            recordatorio2[j] = this.obtener(j);
-        }
-        recordatorio = recordatorio2;
+    public Fecha fechaActual() {
+        return this.fechaActual;
     }
 
-    public void modificarPosicion(int indice, Recordatorio valor) {
-        recordatorio[indice] = valor;
-    }
-
-    public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
-        this.recordatorio = vector.recordatorio.clone();
-        this.longitud = vector.longitud;
-    }
-
-    public ArregloRedimensionableDeRecordatorios copiar() {
-        ArregloRedimensionableDeRecordatorios recordatorio_new = new ArregloRedimensionableDeRecordatorios(this);
-        return recordatorio_new;
-    } 
 }
