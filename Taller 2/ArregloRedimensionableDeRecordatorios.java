9f86a9f4c6ec95a8d94d5457ab3e2878bb7ea7c8
alessia <lima.alessialourdes@gmail.com>
2025-04-01 18:34:47 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- Taller 2/ArregloRedimensionableDeRecordatorios.java
+++ Taller 2/ArregloRedimensionableDeRecordatorios.java
@@ -6,29 +6,38 @@ class ArregloRedimensionableDeRecordatorios {
 
     public ArregloRedimensionableDeRecordatorios() {
         this.longitud = 0;
-        this.recordatorio = Recordatorio[0];
+        this.recordatorio = new Recordatorio[0];
     }
 
     public int longitud() {
-        longitud = this.recordatorio.length;
-        return longitud;
+        return this.recordatorio.length;
     }
 
     public void agregarAtras(Recordatorio i) {
-        int longitud2 = longitud();
         Recordatorio[] recordatorio_new = new Recordatorio[longitud() + 1];
-        for (int j = 0; j < longitud2; j++){
-            recordatorio_new[j] = this.recordatorio[j];
-            longitud += 1;
+        for (int j = 0; j < (longitud()); j++){
+            recordatorio_new[j] = obtener(j);
         }
-        recordatorio_new[longitud] = i;
+        recordatorio_new[recordatorio_new.length-1] = i;
+        this.recordatorio = recordatorio_new.clone();
     }
 
     public Recordatorio obtener(int i) {
-        return recordatorio[i];
+        return this.recordatorio[i];
     }
 
     public void quitarAtras() {
+    
+        if (this.longitud == 0) {
+           // quiero dejarlo como está 
+        }
+        else {
+            Recordatorio[] recordatorio_new = new Recordatorio[this.longitud - 1];
+            for (int j = 0; j < (this.longitud - 1); j++){
+                recordatorio_new[j] = this.recordatorio[j];
+            }
+            this.recordatorio = recordatorio_new.clone();
+        }
         
     }
 
@@ -37,11 +46,17 @@ class ArregloRedimensionableDeRecordatorios {
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
-        // Implementar
+        this.longitud = vector.longitud();
+        this.recordatorio = vector.recordatorio.clone();
     }
 
     public ArregloRedimensionableDeRecordatorios copiar() {
-        // Implementar
-        return null;
+        ArregloRedimensionableDeRecordatorios recordatorios_new = new ArregloRedimensionableDeRecordatorios(this);
+
+        return recordatorios_new;
     }
 }
+
+// consultas: 
+// ver si agenda esta bien 
+// CONSULTAR POR QUITAR ULTIMO Y AGREGAR !!!!!!!!!! 
