a726ab6b46668246b7a2ee24d28d3da9390e92a4
alessia <lima.alessialourdes@gmail.com>
2025-04-01 21:14:32 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- Taller 2/ArregloRedimensionableDeRecordatorios.java
+++ Taller 2/ArregloRedimensionableDeRecordatorios.java
@@ -15,11 +15,12 @@ class ArregloRedimensionableDeRecordatorios {
 
     public void agregarAtras(Recordatorio i) {
         Recordatorio[] recordatorio_new = new Recordatorio[longitud() + 1];
-        for (int j = 0; j < (longitud()); j++){
-            recordatorio_new[j] = obtener(j);
+        for (int j = 0; j < longitud(); j++){
+            recordatorio_new[j] = this.recordatorio[j];
         }
         recordatorio_new[recordatorio_new.length-1] = i;
         this.recordatorio = recordatorio_new.clone();
+        this.longitud = recordatorio_new.length;
     }
 
     public Recordatorio obtener(int i) {
@@ -28,15 +29,16 @@ class ArregloRedimensionableDeRecordatorios {
 
     public void quitarAtras() {
     
-        if (this.longitud == 0) {
-           // quiero dejarlo como está 
+        if (longitud() == 0) {
+           // quiero dejar todo como esta 
         }
         else {
-            Recordatorio[] recordatorio_new = new Recordatorio[this.longitud - 1];
-            for (int j = 0; j < (this.longitud - 1); j++){
+            Recordatorio[] recordatorio_new = new Recordatorio[longitud() - 1];
+            for (int j = 0; j < (longitud() - 1); j++){
                 recordatorio_new[j] = this.recordatorio[j];
             }
             this.recordatorio = recordatorio_new.clone();
+            this.longitud = recordatorio_new.length;
         }
         
     }
