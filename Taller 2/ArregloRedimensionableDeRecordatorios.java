cab4afdb492b4285ed32ad08726b8b4ad31b2311
alessia <lima.alessialourdes@gmail.com>
2025-04-01 23:10:39 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- Taller 2/ArregloRedimensionableDeRecordatorios.java
+++ Taller 2/ArregloRedimensionableDeRecordatorios.java
@@ -1,7 +1,7 @@
 package aed;
 
 class ArregloRedimensionableDeRecordatorios {
-    private int longitud; 
+    private int longitud;
     private Recordatorio[] recordatorio;
 
     public ArregloRedimensionableDeRecordatorios() {
@@ -14,33 +14,28 @@ class ArregloRedimensionableDeRecordatorios {
     }
 
     public void agregarAtras(Recordatorio i) {
-        Recordatorio[] recordatorio_new = new Recordatorio[longitud() + 1];
-        for (int j = 0; j < longitud(); j++){
-            recordatorio_new[j] = this.recordatorio[j];
+        Recordatorio[] recordatorio_new = new Recordatorio[this.longitud() + 1];
+        for (int j = 0; j < this.longitud();j++) {
+            recordatorio_new[j] = this.obtener(j);
         }
         recordatorio_new[recordatorio_new.length-1] = i;
         this.recordatorio = recordatorio_new.clone();
-        this.longitud = recordatorio_new.length;
+        this.longitud += 1;
     }
 
     public Recordatorio obtener(int i) {
-        return this.recordatorio[i];
+        return recordatorio[i];
     }
 
     public void quitarAtras() {
-    
-        if (longitud() == 0) {
-           // quiero dejar todo como esta 
-        }
-        else {
-            Recordatorio[] recordatorio_new = new Recordatorio[longitud() - 1];
-            for (int j = 0; j < (longitud() - 1); j++){
-                recordatorio_new[j] = this.recordatorio[j];
+        if (this.longitud != 0){
+            Recordatorio[] recordatorio_new = new Recordatorio[this.longitud() - 1];
+            for (int j = 0; j < (this.longitud()-1);j++) {
+                recordatorio_new[j] = this.obtener(j);
             }
             this.recordatorio = recordatorio_new.clone();
-            this.longitud = recordatorio_new.length;
+            this.longitud -= 1;
         }
-        
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
@@ -58,7 +53,3 @@ class ArregloRedimensionableDeRecordatorios {
         return recordatorios_new;
     }
 }
-
-// consultas: 
-// ver si agenda esta bien 
-// CONSULTAR POR QUITAR ULTIMO Y AGREGAR !!!!!!!!!! 
