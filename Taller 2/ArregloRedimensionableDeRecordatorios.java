e34f9a39307ca41ad1ae1da2152b31d618301174
alessia <lima.alessialourdes@gmail.com>
2025-04-03 16:16:55 -0300

    Update ArregloRedimensionableDeRecordatorios.java

--- Taller 2/ArregloRedimensionableDeRecordatorios.java
+++ Taller 2/ArregloRedimensionableDeRecordatorios.java
@@ -14,11 +14,11 @@ class ArregloRedimensionableDeRecordatorios {
     }
 
     public void agregarAtras(Recordatorio i) {
-        Recordatorio[] recordatorio_new = new Recordatorio[this.longitud() + 1];
-        for (int j = 0; j < this.longitud();j++) {
-            recordatorio_new[j] = this.obtener(j);
+        Recordatorio[] recordatorio_new = new Recordatorio[this.length + 1];
+        for (int j = 0; j < recordatorio.length;j++) {
+            recordatorio_new[j] = recordatorio[j];
         }
-        recordatorio_new[recordatorio_new.length-1] = i;
+        recordatorio_new[recordatorio.length] = i;
         this.recordatorio = recordatorio_new.clone();
         this.longitud += 1;
     }
@@ -27,15 +27,15 @@ class ArregloRedimensionableDeRecordatorios {
         return recordatorio[i];
     }
 
-    public void quitarAtras() {
-        if (this.longitud != 0){
-            Recordatorio[] recordatorio_new = new Recordatorio[this.longitud() - 1];
-            for (int j = 0; j < (this.longitud()-1);j++) {
-                recordatorio_new[j] = this.obtener(j);
-            }
-            this.recordatorio = recordatorio_new.clone();
-            this.longitud -= 1;
+    public void agregarAtras(Recordatorio i) {
+        Recordatorio[] recordatorio2 = new Recordatorio[recordatorio.length + 1];
+        for (int j = 0; j < recordatorio.length; j++) {
+            recordatorio2[j] = recordatorio[j];
+            
         }
+        recordatorio2[recordatorio2.length] = i;
+        this.longitud += 1; 
+        this.recordatorio = recordatorio2.clone();
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
